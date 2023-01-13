d393de1e2c99875fa6144609fb38937eecaecf23
saulChavin <48201023+saulChavin@users.noreply.github.com>
2023-01-13 14:46:17 -0600

    fix test to deploy

--- src/components/Autocomplete.test.tsx
+++ src/components/Autocomplete.test.tsx
@@ -1,22 +1,9 @@
 import AutoComplete from './Autocomplete'
-import { render, screen, userEvent } from '../utils/test-utils'
+import { render, screen } from '../utils/test-utils'
 
-describe('Simple working test', () => {
-  it('the title is visible', () => {
-    render(<AutoComplete elements={[]}/>)
-    expect(screen.getByText(/Hello Vite \+ React!/i)).toBeInTheDocument()
-  })
-
-  it('should increment count on click', async () => {
-    render(<App />)
-    userEvent.click(screen.getByRole('button'))
-    expect(await screen.findByText(/count is: 1/i)).toBeInTheDocument()
-  })
-
-  it('uses flexbox in app header', async () => {
-    render(<App />)
-    const element = screen.getByRole('banner')
-    expect(element.className).toEqual('App-header')
-    expect(getComputedStyle(element).display).toEqual('flex')
-  })
+it('uses flexbox in app header', async () => {
+  render(<AutoComplete elements={[]} />)
+  const element = screen.getByRole('input')
+  expect(element.attributes).toContain('id')
 })
+
